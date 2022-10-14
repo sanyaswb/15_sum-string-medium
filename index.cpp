6cb516f16f91c6422a4aa29082445dae9be68b84
John Doe <SlySpellar@gmail.com>
2022-10-14 17:34:05 +0300

    removed solution

--- index.cpp
+++ index.cpp
@@ -3,28 +3,5 @@
 #include <cmath>
 
 int sumString(const std::string& str) {
-	int result = 0;
-	bool negative = false;
-
-	for (char ch : str) {
-		if (ch == ',') {
-			continue;
-		}
-
-		if (ch == '-') {
-			negative = true;
-			continue;
-		}
-
-		int converted = ch - '0';
-
-		if (negative) {
-			converted *= -1;
-			negative = false;
-		}
-
-		result += converted;
-	}
-
-	return result;
+	return 0;
 }
