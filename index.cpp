2043c932e089903eed816df045218199c78aa698
sanyaswb <sanyaswb@gmail.com>
2022-10-15 18:57:52 +0300

    completed sum string medium

--- index.cpp
+++ index.cpp
@@ -3,5 +3,28 @@
 #include <cmath>
 
 int sumString(const std::string& str) {
-	return 0;
+	int sum = 0;
+	int negative = false;
+
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == ',') {
+			continue;
+		}
+
+		int code = int(str[i] - '0');
+
+		if (str[i] == '-') {
+			negative = true;
+			continue;
+		}
+
+		if (negative) {
+			code *= -1;
+			negative = false;
+		}
+
+		sum += code;
+	}
+
+	return sum;
 }
